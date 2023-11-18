20d9263a55f250cc28f0ede903f00ccf50849758
Hazem <142467897+HazemHaff@users.noreply.github.com>
2023-11-18 03:18:46 +0100

    Add files via upload

---
+++ #Sampling DataFrames.py
@@ -0,0 +1,7 @@
+#There are many reasons to consider random samples from DataFrames:
+#For large data, a random sample can make computation easier.
+#We may want to train models on a random sample of the data.
+#We may want to over- or under-sample observations when outcomes are uneven.
+
+# Sample 5 rows without replacement
+sample data. sample (n=5, replace=False) print (sample.iloc[:,-3:1)
